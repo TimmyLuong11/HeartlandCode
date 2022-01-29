1ec107eb5df0811aa53cf1b8f7d724da6a6e9972
TimmyLuong <timmyluong11@gmail.com>
2022-01-29 14:49:19 -0600

    Array question

---
+++ Heartland/Heartland/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Heartland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //array out of bounds
+            int[] i = new int[0];
+            Console.WriteLine(i[0]);
+        }
+    }
+}
